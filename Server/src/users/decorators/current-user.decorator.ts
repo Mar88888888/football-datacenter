7c99a48e4e1ffd1cb59c1b82c0583518c0c574aa
Artem <76451013+Mar88888888@users.noreply.github.com>
2024-08-11 13:34:29 +0300

    Favteams/comps

--- Server/src/users/decorators/current-user.decorator.ts
+++ Server/src/users/decorators/current-user.decorator.ts
@@ -3,7 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    console.dir(request);
     return request.currentUser;
   },
 );
