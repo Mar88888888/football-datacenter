515d9b5a20ea71196589e334c9085569ed471f11
Artem <76451013+Mar88888888@users.noreply.github.com>
2025-10-09 14:58:27 +0300

    updated match model

--- Server/src/matches/match.ts
+++ Server/src/matches/match.ts
@@ -1,43 +1,14 @@
 import { Competition } from '../competition/competition';
 import { Team } from '../team/team';
+import { Score } from './score';
 
 export class Match {
   constructor(
-    private id: number,
-    private homeScore: number,
-    private awayScore: number,
-    private homeTeam: Team,
-    private awayTeam: Team,
-    private competition: Competition,
-    private startDate: Date,
-  ){}
-  
-  getId(): number{
-    return this.id;
-  }
-  
-  getHomeScore(): number{
-    return this.homeScore;
-  }
-
-  getAwayScore(): number{
-    return this.awayScore;
-  }
-
-  getHomeTeam(): Team{
-    return this.homeTeam;
-  }
-
-  getAwayTeam(): Team{
-    return this.awayTeam;
-  }
-
-  getCompetition(): Competition{
-    return this.competition;
-  }
-
-  getStartDate(): Date{
-    return this.startDate;
-  }
-
+    public id: number,
+    public score: Score,
+    public homeTeam: Team,
+    public awayTeam: Team,
+    public competition: Competition,
+    public startDate: Date,
+  ) {}
 }
